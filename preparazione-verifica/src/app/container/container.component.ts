f7ea2f9a5f5f3c244a06bb301663cd633f496c91
andreagiodi <giodice.andrea@iisgalvanimi.edu.it>
2022-10-06 09:20:43 +0000

    punto 2

--- preparazione-verifica/src/app/container/container.component.ts
+++ preparazione-verifica/src/app/container/container.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
-
-  constructor() { }
+  macchine: string[];
+  constructor() {
+    this.macchine = ['Ruspa','Autocarro','Gru','Terna','Escavatore','Finitrice','Trencher','Betoniera','Dumper','Compressore']
+   }
 
   ngOnInit(): void {
   }
