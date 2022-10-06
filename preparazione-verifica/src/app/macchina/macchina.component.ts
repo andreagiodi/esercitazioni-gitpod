e794abd632e95ddc5382cee4edaa18b26ae30075
andreagiodi <giodice.andrea@iisgalvanimi.edu.it>
2022-10-06 10:09:30 +0000

    punto 7

--- preparazione-verifica/src/app/macchina/macchina.component.ts
+++ preparazione-verifica/src/app/macchina/macchina.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./macchina.component.css']
 })
 export class MacchinaComponent implements OnInit {
-  @Input() "name": string;
+  @Input() i: string='';
+  pezzi: string[];
+
   constructor() { 
-    
+    this.pezzi = ['Bulloni','Viti','Ferro']
   }
 
   ngOnInit(): void {
