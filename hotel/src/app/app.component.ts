fadfacbedbcba1c9b9fc4ba41125e3e79064555c
andreagiodi <andrea.giodice5@gmail.com>
2022-12-09 14:13:38 +0000

    commit

--- hotel/src/app/app.component.ts
+++ hotel/src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Booking } from './models/booking.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { //implements Oninit
   title = 'hotel';
+  obsRooms! : Observable<Booking[]>
+  data! : Booking[];
+  constructor(private http : HttpClient) 
+  {
+
+  }
+  //----------------------------20:41 minuti del video tutorial------------
+  ngOnInit(): void {
+    this.obsRooms = this.http.get<Booking[]>('https://my-json-server.typicode.com/malizia-g/hotel/booking');
+    this.obsRooms.subscribe(this.fatiqualcosa)
+
+  }
+  fatiqualcosa = (data : Booking[]) => {
+    this.data = data;
+  }
 }
---
+++ hotel/src/app/models/booking.model.ts
@@ -0,0 +1,10 @@
+import { Room } from "./room.model";
+
+export class Booking 
+{
+    from: string = ""; 
+    to: string = "";
+    name: string = "";
+    surname: string = "";
+    room! : Room;
+}
