33b75f25f44dd8c3924b4891e51acbb76c46e267
andreagiodi <andrea.giodice5@gmail.com>
2023-11-09 13:01:18 +0000

    commit

--- pokemon/src/app/api.service.ts
+++ pokemon/src/app/api.service.ts
@@ -18,6 +18,11 @@ export class ApiService {
   getLastpart(arg: string) {
     return arg.split("/")[6]
   }
+
+  getSpecificCategory(arg: string) {
+    const url = 'https://pokeapi.co/api/v2/type/' + arg
+    return this.http.get(url)
+  }
   /*
   getBeersByName(name : string) {
     const url = `https://api.punkapi.com/v2/beers?beer_name=${name}`
--- pokemon/src/app/specific-item/specific-item.component.ts
+++ pokemon/src/app/specific-item/specific-item.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Observable } from 'rxjs';
+import {Location} from '@angular/common'
 
 @Component({
   selector: 'app-specific-item',
@@ -6,5 +10,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./specific-item.component.css']
 })
 export class SpecificItemComponent {
+  routeObs!: Observable<ParamMap>; 
+  obsBeers! : any
+  data! : any
 
+  results : any; 
+  
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router, 
+    private location: Location,
+    public service: ApiService ) { }
+
+
+  ngOnInit(): void {
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
+    
+  }
+
+  getRouterParam = (params: ParamMap) =>
+  {
+    let ItemId : any = params.get('name'); 
+    
+    this.obsBeers = this.service.getSpecificCategory(ItemId)
+    this.obsBeers.subscribe((data: any) => { this.results = data; console.log(this.results) });
+    console.log(this.results)
+  }
+
+ 
+
+  back(){ 
+    this.location.back();
+  }
+
+
+  getLastPart(arg: string) {
+    return arg.split("/")[6]
+  }
+
+  
 }
